007577c570948e1309bb65de00752139d964d0cd
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2024-01-29 22:38:58 +0530

    prime number cheker

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenumber_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string again;
+            do { 
+            Console.Write("Enter a number: ");
+                int num;
+               
+
+                while (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number : ");
+                }
+
+
+                if (prime(num))
+            {
+                Console.WriteLine($"{num} is a prime number.");
+
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a prime number.");
+              
+            }
+                Console.Write("Do you want to check another number? (Y/N): ");
+                again = Console.ReadLine();
+
+                while (!(again.ToUpper() == "Y" || again.ToUpper() == "N"))
+                {
+                    Console.Write("Invalid input. Please enter Y or N: ");
+                    again = Console.ReadLine();
+                }
+
+                if (again.ToUpper() == "N")
+                {
+                    Console.WriteLine("Please press enter key to end...");
+                    Console.ReadLine();
+                }
+
+            } while (again.ToUpper() == "Y");         
+        }
+
+        static bool prime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+            
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("primenumber_checker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("primenumber_checker")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2e193a6-dd50-4726-8ed9-442851734d93")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
